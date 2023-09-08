0211364e237f5b01e5eada9ebc82f533334dcb04
B-KXC18 <143626468+B-KXC18@users.noreply.github.com>
2023-09-08 19:06:47 -0400

    Create config.tf

---
+++ config.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+ 5 changes: 5 additions & 0 deletions5  
