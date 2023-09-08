22df714eb4c4df7b371765658bfec413970c98ee
B-KXC18 <143626468+B-KXC18@users.noreply.github.com>
2023-09-08 19:40:02 -0400

    Update 4main.tf

--- modules/aws-account/4main.tf
+++ modules/aws-account/4main.tf
@@ -1,7 +1,10 @@
   source        = "./modules/aws-account"
   account_name  = "my-account-name"
   email_address = "my-team@my-company.com"
-  owner_users   = ["some-iam-username"]
+  owner_users   = ["some-iam-username"] 
+# users who have admnistrative privileges in the root account
   dev_users     = ["developer-a", "developer-b"]
+# users who have development related roles in the account
   reader_users  = ["junior-developer-x", "manager-y"]
+# users who have read-only access roles in the account
 }
